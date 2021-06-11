d4da18557632525107c97482e1700ec8205efbe5
Victoria A <victoria.arriola@northwestern.edu>
2021-06-10 22:36:55 -0500

    starting

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,38 @@
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+
+d3.json(queryUrl).then(function (data) {
+    createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+    function onEachFeature(feature, layer) {
+
+    }
+};
+
+function createMap(earthquakes) {
+    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      });
+    var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+      });
+    
+    var baseMaps = {
+        "Street Map": street,
+        "Topographic Map": topo
+      };
+      
+    var myMap = L.map("map", {
+        center: [
+          37.09, -95.71
+        ],
+        zoom: 5,
+        layers: [street, earthquakes]
+      });
+
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+      }).addTo(myMap);
+}

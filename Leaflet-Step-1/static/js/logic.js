da1ea134d4efcce53bdcf0d73f6747ddd80e555f
Victoria A <victoria.arriola@northwestern.edu>
2021-06-11 00:23:20 -0500

    troubles

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -3,13 +3,14 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 d3.json(queryUrl).then(function (data) {
     createFeatures(data.features);
+    
 });
 
-function createFeatures(earthquakeData) {
-    function onEachFeature(feature, layer) {
+function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  }
 
-    }
-};
 
 function createMap(earthquakes) {
     var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -18,6 +19,10 @@ function createMap(earthquakes) {
     var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
       });
+    
+    var overlayMaps = {
+        Earthquakes: earthquakes
+      };
     
     var baseMaps = {
         "Street Map": street,

158525835797e1b4f930f08b3ee7ece91c9a54de
Victoria A <victoria.arriola@northwestern.edu>
2021-06-11 23:44:14 -0500

    working?

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,50 +1,83 @@
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-
 d3.json(queryUrl).then(function (data) {
-    createFeatures(data.features);
-    
+
+    function chooseColor(magnitude) {
+    switch (magnitude) {
+        case magnitude > 5:
+            return "#ea2c2c";
+        case magnitude > 4:
+            return "#ea822c";
+        case magnitude > 3:
+            return "#ee9c00";
+        case magnitude > 2:
+            return "#eecc00";
+        case magnitude > 1:
+            return "#d4ee00";
+        default:
+            return "#98ee00";
+        } 
+    }
+    function getRadius(magnitude) {
+        if (magnitude == 0) {
+            return 1;
+     }
+     return magnitude *4;
+    }
+
+    function styleFormat(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: chooseColor(feature.properties.mag),
+            color: "#000000",
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        }
+    }
+
+    var earthquakeLayer = L.geoJson(data, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+    style: styleFormat,
+    onEachFeature: function(feature,layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+    });
+    createMap(earthquakeLayer);
 });
 
-function createFeatures(earthquakeData) {
 
-    function onEachFeature(feature, layer) {
-      layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-    }
-  
-  
-    var earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature
-    });
-    createMap(earthquakes);
-}
-function createMap(earthquakes) {
-    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      });
-    var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-      });
+ 
     
-    var overlayMaps = {
-        Earthquakes: earthquakes
-      };
-    
-    var baseMaps = {
-        "Street Map": street,
-        "Topographic Map": topo
-      };
-      
-    var myMap = L.map("map", {
-        center: [
-          37.09, -95.71
-        ],
-        zoom: 5,
-        layers: [street, earthquakes]
-      });
+    function createMap(earthquakes) {
+        var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        });
+        var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+        });
+        
+        var overlayMaps = {
+            Earthquakes: earthquakes
+        };
+        
+        var baseMaps = {
+            "Street Map": street,
+            "Topographic Map": topo
+        };
+        
+        var myMap = L.map("map", {
+            center: [
+            37.09, -95.71
+            ],
+            zoom: 5,
+            layers: [street, earthquakes]
+        });
 
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-      }).addTo(myMap);
+        L.control.layers(baseMaps, overlayMaps, {
+            collapsed: false
+        }).addTo(myMap);
 }

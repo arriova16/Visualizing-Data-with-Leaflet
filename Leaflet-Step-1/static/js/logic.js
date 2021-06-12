25bc1c422fcb0bdee44b76a861eb1711a3eee0c5
Victoria A <victoria.arriola@northwestern.edu>
2021-06-12 18:31:18 -0500

    wanted to turn something in while i work on legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -78,33 +78,5 @@ d3.json(queryUrl).then(function (data) {
             collapsed: false
         }).addTo(myMap);
 
-        // var legend = L.control({ position: "bottomright" });
-        // legend.onAdd = function() {
-        //   var div = L.DomUtil.create("div", "info legend");
-        //   var limits = geojson.options.limits;
-        //   var colors = geojson.options.colors;
-        //   var labels = [];
-      
-        //   // Add the minimum and maximum.
-        //   var legendInfo = "<h1>Median Income</h1>" +
-        //     "<div class=\"labels\">" +
-        //       "<div class=\"min\">" + limits[0] + "</div>" +
-        //       "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-        //     "</div>";
-      
-        //   div.innerHTML = legendInfo;
-      
-        //   limits.forEach(function(limit, index) {
-        //     labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-        //   });
-      
-        //   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-        //   return div;
-        // };
-      
-        // // Adding the legend to the map
-        // legend.addTo(myMap);
-      
-    
       
 }
